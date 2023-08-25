96df3c8a0e7ced69b020d69fd1781bf6fbf0ef45
wrapperup <wrapperup4@gmail.com>
2023-08-24 23:31:19 -0400

    disable swap files (good riddance)

--- lua/wrap/init.lua
+++ lua/wrap/init.lua
@@ -8,5 +8,6 @@ require("wrap.setup")
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.opt.swapfile = false
 
 -- vim.fn.serverstart('\\\\.\\pipe\\nvim-pipe-1234')
